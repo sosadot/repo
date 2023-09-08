8b88a34c739c87fe1f76d1b3e31027a8ecf9b4a5
Sosa <alandaki@hotmail.com>
2023-09-08 08:15:26 +0200

    test

---
+++ Lektion 1/draw.py
@@ -0,0 +1,22 @@
+import pygame
+
+pygame.init()# Initialize Pygame
+screen = pygame.display.set_mode((640, 480)) # Create a window of 640x480 pixels
+screen.fill((255, 255, 255)) # Fill the screen with white
+
+pygame.draw.line(screen, (0, 0, 0), (100, 380), (540, 380)) # Draw a black line
+
+
+# Draw the ground
+# Draw the bottom of the house
+# Draw two walls
+pygame.draw.line(screen, linecolor, (left, top), (left+bwidth, top), 5)
+# Draw the roof
+
+# Make sure the window stays open until the user closes it
+run_flag = True
+while run_flag is True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run_flag = False
+    pygame.display.flip() # Refresh the screen so drawing appears
